1c72966f06b52594c4f92fc6b5c6a766b863b844
Remix Workspace <>
2022-10-18 22:28:10 +0900

    unChangeableNFTcontents

--- contracts/OffChainChangeableNFT.sol
+++ contracts/OffChainChangeableNFT.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity  ^0.8.14;
+pragma solidity  ^0.8.7;
 
 import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
-
 contract OffChainChangeableNFT is ERC721URIStorage,Ownable{
-    constructor() ERC721("OffChainChangeableNFT","OCCNFT"){
+    constructor() ERC721("OffChainUnchangeableNFT","OCCNFT"){
     }
+
     /**
     *@dev
     *- people be able to mint is only peolpe who deploy this contract; 
     */
-    function  Only_Ownwe_nft_mint(uint256 tokenId) public onlyOwner {
-       _mint(_msgSender(),tokenId);
-    }
-    function setTokenUri(uint256 tokenId,string calldata uri) public onlyOwner{
+    function  Only_Owner_nft_mint(uint256 tokenId, string calldata uri) public onlyOwner {
         _setTokenURI(tokenId,uri);
+       _mint(_msgSender(),tokenId);
     }
 
 
