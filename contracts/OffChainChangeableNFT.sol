f668ce6943b17f612df034818002e70148b28a47
Remix Workspace <>
2022-10-20 20:59:11 +0900

    nft_min

--- contracts/OffChainChangeableNFT.sol
+++ contracts/OffChainChangeableNFT.sol
@@ -5,19 +5,30 @@ pragma solidity  ^0.8.7;
 import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+contract eventNFT is ERC721URIStorage,Ownable{
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
 
-contract OffChainChangeableNFT is ERC721URIStorage,Ownable{
-    constructor() ERC721("OffChainUnchangeableNFT","OCCNFT"){
+    event TokenUriEvent(address indexed sender,uint256 indexed tokenId , string uri);
+
+
+    constructor() ERC721("eventNFT","eventNFT"){
     }
 
     /**
     *@dev
     *- people be able to mint is only peolpe who deploy this contract; 
     */
-    function  Only_Owner_nft_mint(uint256 tokenId, string calldata uri) public onlyOwner {
+    function  Only_Owner_nft_mint(string calldata uri) public onlyOwner {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
         _setTokenURI(tokenId,uri);
        _mint(_msgSender(),tokenId);
+       emit TokenUriEvent(_msgSender(),tokenId,uri);
     }
 
 
